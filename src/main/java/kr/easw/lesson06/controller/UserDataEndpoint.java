f7ae307723f349180e5f30c3fb92ae93b5abedf8
ohzard <117743902+ohzard@users.noreply.github.com>
2023-12-03 21:14:34 +0900

    Update UserDataEndpoint.java

--- src/main/java/kr/easw/lesson06/controller/UserDataEndpoint.java
+++ src/main/java/kr/easw/lesson06/controller/UserDataEndpoint.java
@@ -1,30 +1,40 @@
 package kr.easw.lesson06.controller;
 
-import lombok.RequiredArgsConstructor;
+import kr.easw.lesson06.service.UserDataService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
-// @RestController 어노테이션을 사용하여 이 클래스가 REST 컨트롤러임을 선언합니다.
 @RestController
-// @RequestMapping 어노테이션을 사용하여 이 클래스의 기반 엔드포인트를 /api/v1/data로 설정합니다.
 @RequestMapping("/api/v1/user")
-// final로 지정된 모든 필드를 파라미터로 가지는 생성자를 생성합니다.
-@RequiredArgsConstructor
 public class UserDataEndpoint {
-    // 원래대로라면 리스트를 통해 JSON에서 사용할 수 있는 형태로 변환해야 하지만, 이번 실습에서는 건너뜁니다.
+
+    private final UserDataService userDataService;
+
+    public UserDataEndpoint(UserDataService userDataService) {
+        this.userDataService = userDataService;
+    }
+
     @GetMapping("/list")
-    public List<String> listUsers() {
-        throw new RuntimeException("이곳에 유저 목록을 반환하는 코드를 작성하십시오.");
+    public ResponseEntity<List<String>> listUserIds() {
+        List<String> userIds = userDataService.getAllUserIds();
+        return ResponseEntity.ok(userIds);
     }
 
     // 원래대로라면 리스트를 통해 JSON에서 사용할 수 있는 형태로 변환해야 하지만, 이번 실습에서는 건너뜁니다.
     @PostMapping("/remove")
-    public ResponseEntity<String> removeUser() {
-        throw new RuntimeException("이곳에 유저를 삭제하는 코드를 작성하십시오.");
+    public ResponseEntity<String> removeUser(@RequestBody Map<String, Object> request) {
+        String userId = request.get("userId").toString();
+        boolean removed = userDataService.removeUserById(userId);
+        if (removed) {
+            return ResponseEntity.ok("User successfully removed.");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found.");
+        }
     }
+
+
 }
